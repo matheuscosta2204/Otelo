ee4f94d732372bd5aac7a3d64575d96f868f3a0a
Matheus Costa <matheuscosta@MBP-de-Matheus.acadoebm.br>
2017-09-14 20:37:05 -0300

    add mesa

--- resources/mesa.py
+++ resources/mesa.py
@@ -1,15 +1,16 @@
 from flask_restful import Resource, reqparse
 from models.mesa import MesaModel
+
 class Mesa(Resource):
 
     parser = reqparse.RequestParser()
     parser.add_argument('number',
-        type=int,
+        type=Integer,
         required=True,
         help="This field cannot be blank."
     )
     parser.add_argument('nmb_places',
-        type=int,
+        type=Integer,
         required=True,
         help="This field cannot be blank"
     )
