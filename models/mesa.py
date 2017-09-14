72a1c069ce02af5d2709a344bf1efdc012132ceb
Matheus Costa <matheuscosta@MBP-de-Matheus.acadoebm.br>
2017-09-14 20:31:39 -0300

    add mesa

--- models/mesa.py
+++ models/mesa.py
@@ -1,29 +1,29 @@
 from db import db
 
-class MesaModel(db.Model):
-    __tablename__ = 'mesas'
-
-    id = db.Column(db.Integer, primary_key=True)
-    number = db.Column(db.Integer)
-    nmb_places = db.Column(db.Integer)
-    status = db.Column(db.String(80))
-
-    def __init__(self, number, nmb_places, status):
-        self.number = number
-        self.nmb_places = nmb_places
-        self.status = status
-
-    def json(self):
-        return {'number': self.number, 'nmb_places': self.nmb_places, 'status': self.status}
-
-    def save_to_db(self):
-        db.session.add(self)
-        db.session.commit()
-
-    @classmethod
-    def find_by_number(cls, number):
-        return cls.query.filter_by(number=number).first()
-
+#class MesaModel(db.Model):
+#    __tablename__ = 'mesas'
+#
+#    id = db.Column(db.Integer, primary_key=True)
+#    number = db.Column(db.Integer)
+#    nmb_places = db.Column(db.Integer)
+#    status = db.Column(db.String(80))
+#
+#    def __init__(self, number, nmb_places, status):
+#        self.number = number
+#        self.nmb_places = nmb_places
+#        self.status = status
+#
+#    def json(self):
+#        return {'number': self.number, 'nmb_places': self.nmb_places, 'status': self.status}
+#
+#    def save_to_db(self):
+#        db.session.add(self)
+#        db.session.commit()
+#
+#    @classmethod
+#    def find_by_number(cls, number):
+#        return cls.query.filter_by(number=number).first()
+#
     #@classmethod
     #def count_disponible(cls):
     #    return db.session.query(
