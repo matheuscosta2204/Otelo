4e3c63079db19626d9d7ea42489a27c44121cc94
Matheus Costa <matheuscosta@MBP-de-Matheus.acadoebm.br>
2017-09-14 20:41:08 -0300

    add mesa

--- models/mesa.py
+++ models/mesa.py
@@ -1,18 +1,18 @@
 from db import db
 
-#class MesaModel(db.Model):
-#    __tablename__ = 'mesas'
-#
-#    id = db.Column(db.Integer, primary_key=True)
-#    number = db.Column(db.Integer)
-#    nmb_places = db.Column(db.Integer)
-#    status = db.Column(db.String(80))
-#
-#    def __init__(self, number, nmb_places, status):
-#        self.number = number
-#        self.nmb_places = nmb_places
-#        self.status = status
-#
+class MesaModel(db.Model):
+    __tablename__ = 'mesas'
+
+    id = db.Column(db.Integer, primary_key=True)
+    number = db.Column(db.Integer)
+    nmb_places = db.Column(db.Integer)
+    status = db.Column(db.String(80))
+
+    def __init__(self, number, nmb_places, status):
+        self.number = number
+        self.nmb_places = nmb_places
+        self.status = status
+
 #    def json(self):
 #        return {'number': self.number, 'nmb_places': self.nmb_places, 'status': self.status}
 #
--- resources/mesa.py
+++ resources/mesa.py
@@ -1,5 +1,5 @@
 from flask_restful import Resource, reqparse
-#from models.mesa import MesaModel
+from models.mesa import MesaModel
 
 class Mesa(Resource):
 
