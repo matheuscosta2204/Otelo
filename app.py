b91e47779ba5c9626dd6548e2f8ae3de4a0319ea
Matheus Costa <matheuscosta@MacBook-Pro-de-Matheus.local>
2017-09-24 10:02:02 -0300

    add mesa

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from resources.user import UserRegister
 from resources.mesa import Mesa, MesaDisponible
 
 app = Flask(__name__)
-app.config['DEBUG'] = True
+app.config['DEBUG'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL','sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'EiEi0'
