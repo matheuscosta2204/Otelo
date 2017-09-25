339162d377b0b606657c5d70d076932d7533f6cb
Matheus Costa <matheuscosta@MacBook-Pro-de-Matheus.local>
2017-09-25 19:32:18 -0300

    add mesa

--- models/mesa.py
+++ models/mesa.py
@@ -35,7 +35,6 @@ class MesaModel(db.Model):
         return cls.query.filter_by(number=number).first()
 
     @jwt_required()
-    @classmethod
-    def find_all_mesas_disponible(cls):
-        qry = cls.query.filter_by(status='livre').all()
-        return {"Mesas": [{"disponible": cls.query.filter_by(status='livre').filter_by(nmb_places=mesa.nmb_places).count(), "nmb_places": mesa.nmb_places} for mesa in qry]}
+    def find_all_mesas_disponible():
+        qry = db.query.filter_by(MesaModel.status='livre').all()
+        return {"Mesas": [{"disponible": db.query.filter_by(MesaModel.status='livre').filter_by(MesaModel.nmb_places=mesa.nmb_places).count(), "nmb_places": mesa.nmb_places} for mesa in qry]}
